ddf01161a2227c5e7860aaf442a82a8c6cdf12fb
accolombini <accolombini@gmail.com>
2023-01-26 16:04:20 -0300

    Ativando o Redux Dev Tools no arquivo store.js

--- src/store/store.js
+++ src/store/store.js
@@ -4,6 +4,7 @@ import { reducers } from '../reducers';  // Aqui fazemos o import de todos os Re
 
 // É dentro da store que passamos todos os Redux
 // Nesta store trabalhamos com todos os Reducers
+// Para usar Redux Dev tools --> além de instalara essa extensão no navegador é preciso ativá-lo aqui na store, veja a ativação a seguir
 
 const store = createStore(
   reducers,
