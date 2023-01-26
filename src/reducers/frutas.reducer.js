777378e9b8383070ee45f22f89cb6508ab930870
accolombini <accolombini@gmail.com>
2023-01-26 15:46:20 -0300

    Gerada aplicação para inserir e remover frutas usando Hooks

--- src/reducers/frutas.reducer.js
+++ src/reducers/frutas.reducer.js
@@ -4,6 +4,7 @@ const INITIAL_STATE = {
   frutas: [],
 };
 
+// Lembre-se sempre que no Reducers sempre será necessário retornar um novo estado
 const reducers = (state = INITIAL_STATE, action) => {
   switch (action.type) {
     case actionsTypes.ADICIONAR_FRUTA:
